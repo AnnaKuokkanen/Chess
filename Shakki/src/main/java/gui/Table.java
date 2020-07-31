34ce68270f51066bdad534341323c1bfea5f6e4b
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-01 00:50:56 +0300

    käyttöliittymän toteuttamisen kokeilu

--- Shakki/src/main/java/gui/Table.java
+++ Shakki/src/main/java/gui/Table.java
@@ -1,43 +1,77 @@
 package gui;
 
+import domain.pieces.*;
 import javax.swing.*;  
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Table extends JFrame {
-    
-    JPanel gamePanel = new JPanel();
-    JButton b = new JButton("Hello world!");
+
     Color dark = Color.BLACK;
     Color light = Color.WHITE;
-    private static final Table INSTANCE = new Table();
+    int width;
+    int height;
+    Piece[][] locations;
+    Pieces pieces = new Pieces();
     
-    public Table() {
-        super("Chess");
-        setSize(800, 800);
-        setResizable(true);
-        
-        gamePanel.add(b);
-        repaint();
-        add(gamePanel);
-        
+    public Table(int height, int width) {
+        pieces.setLocations();
+        locations = pieces.getLocations();
+        this.height = height;
+        this.width = width;
+        setTitle("Chess");
+        //System.out.println("Konstruktori");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(height, width);
+        setResizable(true);
+        repaint();
+        
         setVisible(true);
     }
     
-    public Table get() {
-        return this.INSTANCE;
-    }
-//    
-//    public void visible() {
-//        System.out.println("Frame haetaan");
-//        gameFrame.setVisible(true);
-//    }
-//    
     @Override
     public void paint(Graphics g) {
-        System.out.println("Piirretään");
-        super.paintComponents(g);
-        g.setColor(dark);
-        g.fillRect(0, 0, 100, 100);
+        //System.out.println("Piirretään");
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                if((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+                    g.setColor(dark);
+                } else {
+                    g.setColor(light);
+                }
+                int rectHeight = height/8;
+                int rectWidth = width/8;
+                g.fillRect(rectHeight * i, rectWidth * j, rectHeight, rectWidth);
+                if(locations[i][j] instanceof King) {
+                    
+                }
+            }
+        }
+        g.drawImage(findPiece(), 10, 10, this);
+    }
+    
+    public final Image findPiece(Strig file) {
+        try {
+            final BufferedImage image = ImageIO.read(new File("resources/BKing.png"));
+            final ImageIcon ic = new ImageIcon(image);
+            final JLabel imageLabel = new JLabel(ic);
+            //System.out.println("Palautetaan label");
+            return image;
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+        //System.out.println("Palautetaan null");
+        return null;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
