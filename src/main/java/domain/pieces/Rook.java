68b9d7058afa0648410eeba99804f6a90f678505
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-02 16:54:57 +0300

    lisätty tornille metodi, joka palauttaa sallitut siirrot

--- src/main/java/domain/pieces/Rook.java
+++ src/main/java/domain/pieces/Rook.java
@@ -1,7 +1,7 @@
 package domain.pieces;
 
 import chess.model.Side;
-import domain.board.Tile;
+import domain.board.*;
 import java.util.ArrayList;
 
 public class Rook extends Piece {
@@ -13,10 +13,71 @@ public class Rook extends Piece {
         super(side, "rook");
     }
     
-    public ArrayList<Tile> getPossibleMoves() {
-        ArrayList<Tile> tiles = new ArrayList<>();
+    /**
+     * rook can move and capture vertically and horizontally but cannot jump over same side pieces
+     * @param board for current game situation
+     * @return list of tiles where rook can legally move
+     */
+    public ArrayList<Tile> getPossibleMoves(Board board) {
+        ArrayList<Tile> moves = new ArrayList<>();
+        Tile[][] tiles = board.getBoard();
         
-        return tiles;
+        int x = this.getLocation().getX();
+        int y = this.getLocation().getY();
+        
+        int i = 1;
+        while (x + i < 8) {
+            if (tiles[x + i][y] != null && tiles[x + i][y].getPiece().getSide() == this.side) {
+                break;
+            } else if (tiles[x + i][y] != null && tiles[x + i][y].getPiece().getSide() != this.side) {
+                moves.add(tiles[x + i][y]);
+                break;
+            } else {
+                moves.add(tiles[x + i][y]);
+            }
+            i++;
+        }
+        
+        i = 1;
+        while (x - i >= 0) {
+            if (tiles[x - i][y] != null && tiles[x - i][y].getPiece().getSide() == this.side) {
+                break;
+            } else if (tiles[x - i][y] != null && tiles[x - i][y].getPiece().getSide() != this.side) {
+                moves.add(tiles[x - i][y]);
+                break;
+            } else {
+                moves.add(tiles[x - i][y]);
+            }
+            i++;
+        }
+        
+        i = 1;
+        while (y + i < 8) {
+            if (tiles[x][y + i] != null && tiles[x][y + i].getPiece().getSide() == this.side) {
+                break;
+            } else if (tiles[x][y + i] != null && tiles[x][y + i].getPiece().getSide() != this.side) {
+                moves.add(tiles[x][y + i]);
+                break;
+            } else {
+                moves.add(tiles[x][y + i]);
+            }
+            i++;
+        }
+        
+        i = 1;
+        while (y - i >= 0) {
+            if (tiles[x][y - i] != null && tiles[x][y - i].getPiece().getSide() == this.side) {
+                break;
+            } else if (tiles[x][y - i] != null && tiles[x][y - i].getPiece().getSide() != this.side) {
+                moves.add(tiles[x][y - i]);
+                break;
+            } else {
+                moves.add(tiles[x][y - i]);
+            }
+            i++;
+        }
+        
+        return moves;
     }
     
     public int getValue() {
