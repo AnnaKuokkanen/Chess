fafd1b52c0f7491320352593e718eee2deca6e87
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-10 19:30:06 +0300

    Omaan bottiin rivi, joka hakee mustan puolen mahdolliset liikkeet ja palauttaa näistä ensimmäisen listassa

--- src/main/java/chess/bot/MyBot.java
+++ src/main/java/chess/bot/MyBot.java
@@ -2,10 +2,15 @@ package chess.bot;
 
 import chess.engine.GameState;
 import domain.board.Board;
+import domain.board.Tile;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MyBot implements ChessBot {
     
     private final Board board;
+    private ArrayList<Tile> moves;
+    private Random random;
     
     public MyBot() {
         this.board = new Board();
@@ -13,6 +18,7 @@ public class MyBot implements ChessBot {
     
     @Override
     public String nextMove(GameState gamestate) {
+        //return this.board.getPossibleMoves().get(0);
         return "e7e5";
     }
 }
