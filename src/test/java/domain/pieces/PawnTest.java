8154383739fd26e37848a28a9cbf5be43d4cfcfe
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-10 22:48:50 +0300

    Korjataan sotilaan testejä luokkaan tehtyjen korjausten perusteella

--- src/test/java/domain/pieces/PawnTest.java
+++ src/test/java/domain/pieces/PawnTest.java
@@ -12,11 +12,22 @@ public class PawnTest {
     private Pawn blackPawn;
     private Pawn whitePawn;
     private Board board;
+    private ArrayList<Tile> blackMoves;
+    private ArrayList<Tile> whiteMoves;
     
     @Before
     public void setUp() {
         this.whitePawn = new Pawn(Side.WHITE);
         this.blackPawn = new Pawn(Side.BLACK);
+        
+        this.board = new Board();
+        this.board.setupBoard();
+        
+        this.board.getBoard()[6][6].setPiece(whitePawn);
+        this.board.getBoard()[1][1].setPiece(blackPawn);
+        
+        this.blackMoves = new ArrayList<>();
+        this.whiteMoves = new ArrayList<>();
     }
     
     @Test
@@ -26,68 +37,84 @@ public class PawnTest {
     }
     
     @Test
-    public void doesPawnMoveForwardWhenThereIsSpace() {
-        this.board = new Board();
-        this.board.setupBoard();
-        ArrayList<Tile> moves = new ArrayList<>();
-        
-        this.board.getBoard()[1][1].setPiece(whitePawn);
-        
+    public void doesPawnMoveForwardWhenThereIsSpace() {        
         for (Tile tile : whitePawn.getPossibleMoves(board)) {
-            moves.add(tile);
+            whiteMoves.add(tile);
         }
-        assertTrue(moves.size() == 1);
-        assertTrue(moves.contains(new Tile(1, 2)));
+        assertTrue(whiteMoves.size() == 1);
+        assertTrue(whiteMoves.contains(new Tile(6, 5)));
+        
+        for (Tile tile : blackPawn.getPossibleMoves(board)) {
+            blackMoves.add(tile);
+        }
+        assertTrue(blackMoves.size() == 1);
+        assertTrue(blackMoves.contains(new Tile(1, 2)));
     }
     
     @Test
     public void doesPawnStayIfThereIsPieceInFront() {
-        this.board = new Board();
-        this.board.setupBoard();
-        ArrayList<Tile> moves = new ArrayList<>();
-        
-        this.board.getBoard()[1][1].setPiece(whitePawn);
-        this.board.getBoard()[1][2].setPiece(new Knight(Side.WHITE));
+        this.board.getBoard()[1][2].setPiece(new Pawn(Side.WHITE));
+        this.board.getBoard()[6][5].setPiece(new Pawn(Side.WHITE));
         
         for (Tile tile : whitePawn.getPossibleMoves(board)) {
-            moves.add(tile);
+            whiteMoves.add(tile);
         }
-        assertTrue(moves.isEmpty());
+        
+        assertTrue(whiteMoves.isEmpty());
+        
+        for (Tile tile : blackPawn.getPossibleMoves(board)) {
+            blackMoves.add(tile);
+        }
+        
+        assertTrue(blackMoves.isEmpty());
     }
     
     @Test
     public void doesPawnMoveDiagonallyWhenThereIsOppositePiece() {
-        this.board = new Board();
-        this.board.setupBoard();
-        ArrayList<Tile> moves = new ArrayList<>();
-        
-        this.board.getBoard()[1][1].setPiece(whitePawn);
-        this.board.getBoard()[0][2].setPiece(new Pawn(Side.BLACK));
-        this.board.getBoard()[1][2].setPiece(new Pawn(Side.WHITE));
-        this.board.getBoard()[2][2].setPiece(new Pawn(Side.BLACK));
-        
-        for (Tile tile : whitePawn.getPossibleMoves(board)) {
-            moves.add(tile);
-        }
-        assertTrue(moves.size() == 2);
-        assertTrue(moves.contains(new Tile(0, 2)));
-        assertTrue(moves.contains(new Tile(2, 2)));
-    }
-    
-    @Test
-    public void doesPawnStayIfThereIsSameSidePieceDiagonally() {
-        this.board = new Board();
-        this.board.setupBoard();
-        ArrayList<Tile> moves = new ArrayList<>();
-        
-        this.board.getBoard()[1][1].setPiece(whitePawn);
         this.board.getBoard()[0][2].setPiece(new Pawn(Side.WHITE));
         this.board.getBoard()[1][2].setPiece(new Pawn(Side.WHITE));
         this.board.getBoard()[2][2].setPiece(new Pawn(Side.WHITE));
         
+        this.board.getBoard()[5][5].setPiece(new Pawn(Side.BLACK));
+        this.board.getBoard()[6][5].setPiece(new Pawn(Side.BLACK));
+        this.board.getBoard()[7][5].setPiece(new Pawn(Side.BLACK));
+        
         for (Tile tile : whitePawn.getPossibleMoves(board)) {
-            moves.add(tile);
+            whiteMoves.add(tile);
         }
-        assertTrue(moves.isEmpty());
+        
+        assertTrue(whiteMoves.size() == 2);
+        assertTrue(whiteMoves.contains(new Tile(5, 5)));
+        assertTrue(whiteMoves.contains(new Tile(7, 5)));
+        
+        for (Tile tile : blackPawn.getPossibleMoves(board)) {
+            blackMoves.add(tile);
+        }
+        assertTrue(blackMoves.size() == 2);
+        assertTrue(blackMoves.contains(new Tile(0, 2)));
+        assertTrue(blackMoves.contains(new Tile(2, 2)));
+    }
+    
+    @Test
+    public void doesPawnStayIfThereIsSameSidePieceDiagonally() {
+        this.board.getBoard()[0][2].setPiece(new Pawn(Side.BLACK));
+        this.board.getBoard()[1][2].setPiece(new Pawn(Side.BLACK));
+        this.board.getBoard()[2][2].setPiece(new Pawn(Side.BLACK));
+        
+        this.board.getBoard()[5][5].setPiece(new Pawn(Side.WHITE));
+        this.board.getBoard()[6][5].setPiece(new Pawn(Side.WHITE));
+        this.board.getBoard()[7][5].setPiece(new Pawn(Side.WHITE));
+        
+        for (Tile tile : whitePawn.getPossibleMoves(board)) {
+            whiteMoves.add(tile);
+        }
+        
+        assertTrue(whiteMoves.isEmpty());
+        
+        for (Tile tile : blackPawn.getPossibleMoves(board)) {
+            blackMoves.add(tile);
+        }
+        
+        assertTrue(blackMoves.isEmpty());
     }
 }
