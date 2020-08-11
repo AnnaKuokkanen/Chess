c16500b60559409ba29a77701674242725ef6f46
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-12 01:47:58 +0300

    Toimiva botti, joka palauttaa ensimmäisen mahdollisen löytyneen siirron

--- src/main/java/chess/bot/MyBot.java
+++ src/main/java/chess/bot/MyBot.java
@@ -7,38 +7,41 @@ import domain.board.Tile;
 import domain.board.TileNameConverter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Random;
 
 public class MyBot implements ChessBot {
     
     private final Board board;
     private HashMap<Tile, ArrayList<Tile>> moves;
-    private Random random;
-    private TileNameConverter converter;
+    private final TileNameConverter converter;
     
     public MyBot() {
         this.board = new Board();
         this.board.setupBoard();
         this.board.setupPieces();
-        this.moves = new HashMap<>();
+        
         this.converter = new TileNameConverter();
     }
     
     @Override
     public String nextMove(GameState gamestate) {
         this.moves = this.board.getPossibleMoves(Side.BLACK);
-        this.random = new Random(moves.keySet().size() - 1);
-        
-        //Tile start = (Tile) moves.keySet().toArray()[random.nextInt()];
-        Tile start = (Tile) moves.keySet().toArray()[0];
-        Tile finish = moves.get(start).get(0);
-        
-        String move = converter.convert(start.getX(), start.getY());
-        move += converter.convert(finish.getX(), finish.getY());
-        
-        finish.setPiece(start.getPiece());
-        start.setPiece(null);
-        
+        String move = "";
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (!moves.get(board.getBoard()[i][j]).isEmpty()) {
+                    Tile start = board.getBoard()[i][j];
+                    Tile finish = moves.get(start).get(0);
+
+                    move += converter.convert(start.getX(), start.getY());
+                    move += converter.convert(finish.getX(), finish.getY());
+
+                    finish.setPiece(start.getPiece());
+                    start.setPiece(null);
+                    break;
+                }
+            }
+            break;
+        }
         return move;
     }
 }
