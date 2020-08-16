dd46a7d12fe474065aff2821f8d30a6a6200b53d
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-16 04:24:31 +0300

    oman listarakenteen rakentaminen

---
+++ src/main/java/datastructureproject/datastructure/ArrayList.java
@@ -0,0 +1,27 @@
+package datastructureproject.datastructure;
+
+public class ArrayList {
+    private Object[] array;
+    private int pointer;
+    
+    public ArrayList() {
+        this.array = new Object[100];
+        this.pointer = 0;
+    }
+    
+    public Object get(int i) {
+        return array[i];
+    }
+    
+    public void add(Object o) {
+        Object[] helperArray = new Object[array.length * 2];
+        if (pointer == array.length - 1) {
+            for (int i = 0; i < array.length; i++) {
+                helperArray[i] = array[i];
+            }
+            array = helperArray;
+        }
+        array[pointer] = o; 
+        pointer++;
+    }
+}
