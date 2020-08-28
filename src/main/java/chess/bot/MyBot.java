cdfb30ec4b669a928e6e163208f6662dd1cb19e5
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-28 03:52:58 +0300

    Javadoc chess-kansion luokalle

--- src/main/java/chess/bot/MyBot.java
+++ src/main/java/chess/bot/MyBot.java
@@ -7,6 +7,10 @@ import domain.board.Board;
 import domain.board.Tile;
 import domain.board.TileNameConverter;
 
+/**
+ * This class implements ChessBot-interface and 
+ * contains method for passing next move to the Bot.
+ */
 public class MyBot implements ChessBot {
 
     private AlphaBetaPruning alphabeta;
@@ -19,6 +23,11 @@ public class MyBot implements ChessBot {
         this.board.setupPieces();
     }
     
+    /**
+     * @param gamestate is pointing to class GameState.
+     * GameState is a premade class and it contains data from the game.
+     * @return move in textual form, for example "e2e4"
+     */
     @Override
     public String nextMove(GameState gamestate) {       
         if (gamestate.getMoveCount() > 0) { 
