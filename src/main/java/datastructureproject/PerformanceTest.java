953e5271e9975c5ebce2509cabd460785c506bf5
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-31 04:56:03 +0300

    Lisätään suorituskykytestiin ajan keskiarvon laskeminen

--- src/main/java/datastructureproject/PerformanceTest.java
+++ src/main/java/datastructureproject/PerformanceTest.java
@@ -16,6 +16,7 @@ public class PerformanceTest {
     public static void main(String[] args) {
         int[] depths = new int[] {1, 2, 3, 4};
         int moves = 20;
+        long[][] times = new long[depths.length][moves];
         
         for (int i = 0; i < depths.length; i++) {
             bot = new MyBot(depths[i]);
@@ -32,7 +33,17 @@ public class PerformanceTest {
                 bot.nextMove(gamestate);
                 long finish = System.nanoTime();
                 System.out.println("Time passed: " + (finish - begin));
+                times[i][j] = finish - begin;
             }
+        }
+        
+        for (int i = 0; i < depths.length; i++) {
+            long sum = 0;
+            for (int j = 0; j < times.length; j++) {
+                sum += times[i][j];
+            }
+            long average = sum / times.length;
+            System.out.println("Average time for depth " + i + " is " + average);
         }
     }
 }
