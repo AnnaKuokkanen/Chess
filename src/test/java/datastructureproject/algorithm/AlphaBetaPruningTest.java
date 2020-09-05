2cc16fbcf03f92c142bd459de4685a97d3969eb3
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-09-05 09:30:08 +0300

    JUnit-testi

--- src/test/java/datastructureproject/algorithm/AlphaBetaPruningTest.java
+++ src/test/java/datastructureproject/algorithm/AlphaBetaPruningTest.java
@@ -20,7 +20,7 @@ public class AlphaBetaPruningTest {
     }
     
     @Test
-    public void testFirstGameSituation() {
+    public void testFirstGameSituationForWhites() {
         board.getBoard()[7][0].setPiece(new Queen(Side.WHITE));
         board.getBoard()[3][5].setPiece(new King(Side.WHITE));
         
@@ -30,6 +30,19 @@ public class AlphaBetaPruningTest {
         AlphaBetaPruning ab = new AlphaBetaPruning(board, 2);
         
         assertEquals("h8e8", ab.useAlphaBetaPruner(Side.WHITE));
+    }
+    
+    @Test
+    public void testFirstGameSituationForBlacks() {
+        board.getBoard()[7][0].setPiece(new Queen(Side.WHITE));
+        board.getBoard()[3][5].setPiece(new King(Side.WHITE));
+        
+        board.getBoard()[4][0].setPiece(new Rook(Side.BLACK));
+        board.getBoard()[6][4].setPiece(new King(Side.BLACK));
+        
+        AlphaBetaPruning ab = new AlphaBetaPruning(board, 2);
+        
+        assertEquals("e8h8", ab.useAlphaBetaPruner(Side.BLACK));
     }
     
     @Test
